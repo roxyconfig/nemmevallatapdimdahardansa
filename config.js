6297dbf3ae50dd07bfe611b16ef8c294f2e9a3ba
roxyconfig <92932568+roxyconfig@users.noreply.github.com>
2022-06-21 12:36:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = ({
-	token: "",//5168518220:AAFgt4FKaeixrzmMmnbIm7HAqEs5JOk1onw
-	botId: ,//5168518220
+	token: "",//5585255248:AAFtM2oIxW8qzKvN88Bjuok3hpxWc9kQ2ro
+	botId: ,//2114930164
 	rounds: 5,//5
 	waitDelay: 12000,//12000
 	timerSteps: 3,//Toxunma
